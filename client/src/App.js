a2dc83e52d1713c3e2fc9f2c83e676aff5eb88d3
Chahat <chahatbashani_it22a8_75@dtu.ac.in>
2024-05-29 17:33:07 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+} from "react-router-dom";
+import Home from './Home';
+import Player from './Player';
+// add below the other import statements
+
+
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/player/:id" component={Player}></Route>
+            </switch>
+        </Router>
+    );
+}
+export default App;
